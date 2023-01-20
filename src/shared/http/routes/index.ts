2a8e7dc03332acd4ce9ad8005375319bbd2438a1
Mariano Silva <mariano.silva@dtidigital.com>
2023-01-20 11:51:23 -0300

    add roles routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import { rolesRouter } from '@roles/http/routes/rolesRoutes'
 
 const routes = Router()
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Olá, Devs!' })
 })
+
+routes.use('/roles', rolesRouter)
 
 export { routes }
