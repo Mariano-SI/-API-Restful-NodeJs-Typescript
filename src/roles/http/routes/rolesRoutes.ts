2d9a44258d04cf19c3225a2f54ed2553a9011fe8
Mariano Silva <mariano.silva@dtidigital.com>
2023-01-20 12:07:48 -0300

    add role entity

--- src/roles/http/routes/rolesRoutes.ts
+++ src/roles/http/routes/rolesRoutes.ts
@@ -1,16 +1,14 @@
+import { Role } from '@roles/entities/Role'
 import { Router } from 'express'
-import { v4 as uuidv4 } from 'uuid'
 
 const rolesRouter = Router()
-const roles = []
+const roles: Role[] = []
 
 rolesRouter.post('/', (req, res) => {
   const { name } = req.body
-  const role = {
-    id: uuidv4(),
-    name,
-    created_at: new Date(),
-  }
+  const role = new Role()
+  role.name = name
+  role.created_at = new Date()
 
   roles.push(role)
 
